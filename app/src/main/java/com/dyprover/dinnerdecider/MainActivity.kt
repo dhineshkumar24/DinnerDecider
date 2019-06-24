3971064b9ef29c05f57e6d2a530f0454472e5d54
dhineshkumar <dhineshkumar.m@happiestminds.com>
2019-06-24 15:29:09 +0530

    Initial commit

---
+++ app/src/main/java/com/dyprover/dinnerdecider/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.dyprover.dinnerdecider
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    val foodList = arrayListOf("abc", "def", "ghi", "jkl", "mno", "pqr", "stu", "vw", "xyz")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        decideBtn.setOnClickListener {
+            val random = Random()
+            val randomFood = random.nextInt(foodList.count())
+            selectedFoodTxt.text = foodList[randomFood]
+        }
+
+        addFoodBtn.setOnClickListener {
+            val newFood = addFoodTxt.text.toString()
+            foodList.add(newFood)
+            addFoodTxt.text.clear()
+        }
+    }
+}
